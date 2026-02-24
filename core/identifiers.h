a93cfcf8252f7e78a449a060838ec579fbc7a19e
Mahmoud <msharkawy005@gmail.com>
2026-02-24 06:02:03 +0200

    Update identifiers.h

--- core/identifiers.h
+++ core/identifiers.h
@@ -1,1 +1,19 @@
+// AuraFix Suite - Hardware Identifiers Constants
+#ifndef IDENTIFIERS_H
+#define IDENTIFIERS_H
+
+// Apple Devices
+#define APPLE_VID 0x05AC
+#define IPHONE_DFU_PID 0x1227
+#define IPHONE_RECOVERY_PID 0x1281
+
+// Qualcomm EDL Mode
+#define QUALCOMM_VID 0x05C6
+#define QUALCOMM_EDL_PID 0x9008
+
+// Samsung Download Mode
+#define SAMSUNG_VID 0x04E8
+#define SAMSUNG_DOWNLOAD_PID 0x685D
+
+#endif
 
